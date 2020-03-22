442c49ef27aa99e90a32b4c6bad87777b188b7cf
eocampo2728 <eduardo.ocampo2728@hotmail.com>
2020-03-22 14:17:36 -0500

    Poblado base

---
+++ dataBase/poblado.sql
@@ -0,0 +1,5 @@
+insert into USR values(0,'admin','David','Navarro','david@mail.com','123','asd','david','+57',310111111,100.12,5);
+insert into USR values(1,'admin','Eduardo','Ocampo','eduardo@mail.com','123','asd','eduardo','+57',310111111,100.12,5);
+insert into USR values(2,'admin','Juan','Villate','juan@mail.com','123','asd','juan','+57',310111111,100.12,5);
+
+insert into USR values(3,'user','Prueba','Prueba','prueba@mail.com','123','asd','prueba','+57',3101111110,0,5);
