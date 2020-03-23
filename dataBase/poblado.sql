57692407fe3b90acda2f34b2f502d8ed0493d366
Eduardo Ocampo <53972469+eocampo2728@users.noreply.github.com>
2020-03-23 11:15:12 -0500

    Update poblado.sql

--- dataBase/poblado.sql
+++ dataBase/poblado.sql
@@ -2,4 +2,24 @@ insert into USR values(0,'admin','David','Navarro','david@mail.com','123','asd',
 insert into USR values(1,'admin','Eduardo','Ocampo','eduardo@mail.com','123','asd','eduardo','+57',310111111,100.12,5);
 insert into USR values(2,'admin','Juan','Villate','juan@mail.com','123','asd','juan','+57',310111111,100.12,5);
 
-insert into USR values(3,'user','Prueba','Prueba','prueba@mail.com','123','asd','prueba','+57',3101111110,0,5);
+insert into USR values(3,'user','Prueba','Prueba','prueba1@mail.com','123','asd','prueba1','+57',3101111110,0,5);
+insert into USR values(4,'user','Prueba','Prueba','prueba2@mail.com','123','asd','prueba2','+57',3101111110,0,5);
+insert into USR values(5,'user','Prueba','Prueba','prueba3@mail.com','123','asd','prueba3','+57',3101111110,0,5);
+insert into USR values(6,'user','Prueba','Prueba','prueba4@mail.com','123','asd','prueba4','+57',3101111110,0,5);
+insert into USR values(7,'user','Prueba','Prueba','prueba5@mail.com','123','asd','prueba5','+57',3101111110,0,5);
+
+
+insert into product values(1,'Libro','Calculo Multivariable, Stewart', 50000,1,null);
+insert into product values(2,'Regla','Regla', 2000,1,null);
+insert into product values(3,'Libro','Calculo Multivariable, Stewart', 50000,2,null);
+insert into product values(4,'Marcador','Marcador rojo', 1500,2,null);
+insert into product values(5,'Cuaderno','Cuaderno cuadriculado 100h', 7000,3,null);
+insert into product values(6,'Borrador','borrador', 1000,3,null);
+insert into product values(7,'Micropunta','Micropunta negro', 2000,4,null);
+insert into product values(8,'Borrador','borrador', 800,4,null);
+insert into product values(9,'Cuaderno','Cuaderno cuadriculado 100h', 8000,5,null);
+insert into product values(10,'Libro','Calculo Multivariable, Stewart', 50000,5,null);
+insert into product values(11,'Micropunta','Micropunta negro', 1000,6,null);
+insert into product values(12,'Borrador','borrador', 1500,6,null);
+insert into product values(13,'Gafas','Gafas de laboratorio', 10000,7,null);
+insert into product values(14,'Regla','Regla', 2000,7,null);
