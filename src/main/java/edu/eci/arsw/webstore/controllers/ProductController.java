fe21d68b3f5ba4a9cf13d645932cb92b55334cc1
Juan Villate <juan.villate-i@mail.escuelaing.edu.co>
2020-03-25 05:31:58 +0530

    POST Product

--- src/main/java/edu/eci/arsw/webstore/controllers/ProductController.java
+++ src/main/java/edu/eci/arsw/webstore/controllers/ProductController.java
@@ -6,18 +6,25 @@
 package edu.eci.arsw.webstore.controllers;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import edu.eci.arsw.webstore.model.Product;
+import edu.eci.arsw.webstore.model.User;
 import edu.eci.arsw.webstore.services.product.ProductServices;
+import edu.eci.arsw.webstore.services.user.UserServices;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,6 +39,9 @@ public class ProductController {
     
     @Autowired
     ProductServices pService;
+    
+    @Autowired
+    UserServices uService;
     
     @RequestMapping(method = RequestMethod.GET, path = "products")
     public ResponseEntity<?> getAllProducts() {
@@ -61,6 +71,42 @@ public class ProductController {
         } catch (Exception ex) {
             Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, ex);
             return new ResponseEntity<>("No se ha podido retornar los productos del usuario con nickname: " + username, HttpStatus.NOT_FOUND);
+        }
+    }
+    
+    @RequestMapping(method = RequestMethod.POST, path = "products")
+    public ResponseEntity<?> createNewUser(@RequestBody String product ) {
+        //Formato de json {"emailUser":email,"passwordUser":password,"username":username}
+        try {
+            //System.out.println("controller: "+user.getUserNickname());
+            //uService.createNewUser(user);
+            System.out.println("producto: "+product);
+            //Pasar el String JSON a un Map
+            Type listType = new TypeToken<Map<Integer, Product>>() {
+            }.getType();
+            Map<String, Product> result = new Gson().fromJson(product, listType);
+            
+            //Obtener las llaves del Map
+            Object[] nameKeys = result.keySet().toArray();
+            
+            Product pd = result.get(nameKeys[0]);
+            
+            
+            
+            ObjectId newObjectIdProduct = new ObjectId(new Date());
+            pd.setProductId(newObjectIdProduct.toHexString());
+            //pd.setProductUser(user);
+            System.out.println("Nombre producto: "+pd.getProductName());
+            System.out.println("vendedor: "+pd.getProductUser());
+            System.out.println("id: "+pd.getProductId());
+            
+            pService.createNewProduct(pd);
+               
+            return new ResponseEntity<>(HttpStatus.CREATED);
+            
+        } catch (Exception ex) {
+            Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("No se ha podido registrar el usuario", HttpStatus.FORBIDDEN);
         }
     }
 }
--- src/main/java/edu/eci/arsw/webstore/persistence/impl/InMemoryProductPersistence.java
+++ src/main/java/edu/eci/arsw/webstore/persistence/impl/InMemoryProductPersistence.java
@@ -9,6 +9,8 @@ import edu.eci.arsw.webstore.model.Product;
 import edu.eci.arsw.webstore.persistence.ProductPersistence;
 import edu.eci.arsw.webstore.persistence.WebStoreDB;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,14 +18,14 @@ import org.springframework.stereotype.Service;
  * @author jmvillatei
  */
 @Service
-public class InMemoryProductPersistence implements ProductPersistence{
-    
+public class InMemoryProductPersistence implements ProductPersistence {
+
     WebStoreDB wsdb;
-    
-    private void newDb(){
+
+    private void newDb() {
         wsdb = new WebStoreDB();
     }
-    
+
     @Override
     public List<Product> getAllProducts() {
         newDb();
@@ -37,9 +39,16 @@ public class InMemoryProductPersistence implements ProductPersistence{
     }
 
     @Override
-    public void createNewProduct(Product pr, String userNickname) {
+    public void createNewProduct(Product pr) {
         newDb();
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        try {
+            wsdb.createNewProduct(pr);
+
+        } catch (Exception ex) {
+            Logger.getLogger(InMemoryProductPersistence.class.getName()).log(Level.SEVERE, null, ex);
+
+        }
     }
 
     @Override
@@ -53,5 +62,5 @@ public class InMemoryProductPersistence implements ProductPersistence{
         newDb();
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
 }
