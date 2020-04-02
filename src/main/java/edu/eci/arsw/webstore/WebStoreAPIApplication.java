7424da850e8c257877827b39db2cff916a00923e
Juan David Navarro Jimenez <juan.navarro@mail.escuelaing.edu.co>
2020-04-02 07:52:10 -0500

    Documentacion API con Swagger

--- src/main/java/edu/eci/arsw/webstore/WebStoreAPIApplication.java
+++ src/main/java/edu/eci/arsw/webstore/WebStoreAPIApplication.java
@@ -5,18 +5,59 @@
  */
 package edu.eci.arsw.webstore;
 
+import com.google.common.base.Predicate;
+import static com.google.common.base.Predicates.or;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import static springfox.documentation.builders.PathSelectors.regex;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  *
  * @author jmvillatei
  */
 @SpringBootApplication
+@Configuration
+@EnableSwagger2
 public class WebStoreAPIApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WebStoreAPIApplication.class, args);
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(webStoreApiInfo())
+                .select()
+                .paths(webStorePaths())
+                .apis(RequestHandlerSelectors.any())
+                .build()
+                .useDefaultResponseMessages(false);
+
+    }
+
+    private ApiInfo webStoreApiInfo() {
+
+        return new ApiInfoBuilder()
+                .title("Service IncogniTrade")
+                .version("1.0")
+                .license("Apache License Version 2.0")
+                .build();
+
+    }
+
+    private Predicate<String> webStorePaths() {
+        return or(
+                regex("/api/v1.*")
+        );
     }
 
 }
